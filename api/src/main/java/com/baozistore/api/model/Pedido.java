d8aa211fe0f9c28eb470fab7d71b6967f70e73ad
Henrique Aguiar <rickdf7@gmail.com>
2026-06-10 13:05:21 -0400

    feat: create entity models for Cliente, Produto and Pedido

---
+++ api/src/main/java/com/baozistore/api/model/Pedido.java
@@ -0,0 +1,10 @@
+package com.baozistore.api.model;
+
+public class Pedido {
+
+    private Long id;
+    private Long clienteId;
+    private Long produtoId;
+    private Integer quantidade;
+    
+}
